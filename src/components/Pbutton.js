a1eca45a19adda78b52ddc4e8bd9a4574bf44911
Akshay Chaudhari <akshaychaudhari1499@gmail.com>
2023-01-07 10:43:41 +0530

    tried using append

--- src/components/Pbutton.js
+++ src/components/Pbutton.js
@@ -3,14 +3,18 @@ import React from 'react'
 
 function Pbutton() {
 
-  const [para, setpara] = useState("")
+  // const [para, setpara] = useState("")
   function paragraph() {
-    setpara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+    // setpara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy") 
+    const para=document.createElement('p') 
+    para.innerHTML = "Hello, I've learnt to use the full-stack evaluation";
+    document.getElementById("para").appendChild(para);
+
   }
   return (<>
   <button id='click' onClick={paragraph}>Click Me</button>
-    <p id='para'>{para}</p>
-  </>
+  <p id="para"></p>
+    </>
   )
 }
 export default Pbutton;
