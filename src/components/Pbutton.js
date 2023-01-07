f8464c61fc14df84e66d6944fc5028f0662fe27a
Akshay Chaudhari <akshaychaudhari1499@gmail.com>
2023-01-07 10:32:21 +0530

    checking

--- src/components/Pbutton.js
+++ src/components/Pbutton.js
@@ -1,16 +1,16 @@
-import { useState } from "react"
-import React from 'react'
+// import { useState } from "react"
+// import React from 'react'
 
-function Pbutton() {
+// function Pbutton() {
 
-  const [para, setpara] = useState("")
-  function paragraph() {
-    setpara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
-  }
-  return (<>
-  <button id='click' onClick={paragraph}>Click Me</button>
-    <p id='para'>{para}</p>
-  </>
-  )
-}
-export default Pbutton;
+//   const [para, setpara] = useState("")
+//   function paragraph() {
+//     setpara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+//   }
+//   return (<>
+//   <button id='click' onClick={paragraph}>Click Me</button>
+//     <p id='para'>{para}</p>
+//   </>
+//   )
+// }
+// export default Pbutton;
