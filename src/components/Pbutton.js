67b47e6841db4cb8782fc5ad02d87c08d9fc0f8b
Akshay Chaudhari <akshaychaudhari1499@gmail.com>
2023-01-07 10:25:05 +0530

    changed sequence

--- src/components/Pbutton.js
+++ src/components/Pbutton.js
@@ -1,16 +1,16 @@
 import { useState } from "react"
-import React  from 'react'
+import React from 'react'
 
 function Pbutton() {
-  
-  const[para,setpara]=useState("")
-      function paragraph(){    
-      setpara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
-      }
-  return ( <>
-     <p id='para'>{para}</p> 
-      <button id='click' onClick={paragraph}>Click Me</button>
-      </>
-        )
+
+  const [para, setpara] = useState("")
+  function paragraph() {
+    setpara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+  }
+  return (<>
+  <button id='click' onClick={paragraph}>Click Me</button>
+    <p id='para'>{para}</p>
+  </>
+  )
 }
 export default Pbutton;
