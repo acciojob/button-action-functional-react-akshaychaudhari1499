c2192e3b3369b7fa1d0199a8d4745d505c301156
Akshay Chaudhari <akshaychaudhari1499@gmail.com>
2023-01-07 10:50:43 +0530

    state

--- src/components/Pbutton.js
+++ src/components/Pbutton.js
@@ -3,17 +3,17 @@ import React from 'react'
 
 function Pbutton() {
 
-  // const [para, setpara] = useState("")
+  const [para, setpara] = useState("")
   function paragraph() {
-    // setpara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy") 
-    const para=document.getElementById('para') 
-    para.innerText = "Hello, I've learnt to use the full-stack evaluation";
+    setpara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy") 
+    // const para=document.getElementById('para') 
+    // para.innerText = "Hello, I've learnt to use the full-stack evaluation";
   
 
   }
   return (<>
   <button id='click' onClick={paragraph}>Click Me</button>
-  <p id="para"></p>
+  <p id="para">{para}</p>
     </>
   )
 }
